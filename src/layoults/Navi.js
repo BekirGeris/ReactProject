a6478a2ce9dd5906ad48e18e3fd886077099a368
bekir geriş <bekir.geris@gmail.com>
2021-08-02 12:55:11 +0300

    kampın 13.günü tamamlandı.👍✔👏

--- src/layoults/Navi.js
+++ src/layoults/Navi.js
@@ -4,8 +4,10 @@ import { Button, Container, Menu } from 'semantic-ui-react'
 import SignedOut from './SignedOut'
 import SignnedIn from './SignnedIn'
 import { useHistory } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 export default function Navi() {
+  const { cartItems } = useSelector(state => state.cart)
 
   const [isAuthenticaten, setIsAuthenticaten] = useState(true)
 
@@ -35,7 +37,7 @@ export default function Navi() {
 
 
           <Menu.Menu position='right'>
-              <CartSummary/>
+              {cartItems.length>0 && <CartSummary/>}
               {isAuthenticaten ? <SignnedIn signOut={handleSignOut} /> : <SignedOut signIn={handleSignIn} />}
           </Menu.Menu>
         </Container>
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Icon, Menu, Table } from 'semantic-ui-react'
+import { Button, Icon, Menu, Table } from 'semantic-ui-react'
 import ProductService from '../services/productService'
+import { useDispatch } from 'react-redux'
+import { addToCart } from '../store/actions/cartActions'
+import { toast } from 'react-toastify'
 
 export default function ProductList() {
 
     const [products, setProducts] = useState([])
 
-    useEffect(()=>{
-        let productService  = new ProductService()
-        productService.getProducts().then(result=>setProducts(result.data.data))
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        let productService = new ProductService()
+        productService.getProducts().then(result => setProducts(result.data.data))
     }, [])
+
+    const handleAddToCart = (product) => {
+        dispatch(addToCart(product));
+        toast.success(`${product.productName} sepete eklendi!`);
+    };
 
     return (
         <div>
@@ -22,6 +32,9 @@ export default function ProductList() {
                         <Table.HeaderCell>Stok Adeti</Table.HeaderCell>
                         <Table.HeaderCell>Açıklama</Table.HeaderCell>
                         <Table.HeaderCell>Kategori</Table.HeaderCell>
+                        <Table.HeaderCell>
+
+                        </Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
@@ -34,6 +47,9 @@ export default function ProductList() {
                                 <Table.Cell>{product.unitsInStock}</Table.Cell>
                                 <Table.Cell>{product.quantityPerUnit}</Table.Cell>
                                 <Table.Cell>{product.category.categoryName}</Table.Cell>
+                                <Table.Cell>
+                                    <Button onClick={() => handleAddToCart(product)}>Sepete ekle</Button>
+                                </Table.Cell>
                             </Table.Row>
                         ))
                     }
